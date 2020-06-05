ebc346fb552ae3150523d522ff7cef47f71a3b93
Ian Langworth <ian@langworth.com>
2020-06-05 15:25:19 -0700

    Improve click action for popups

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,12 +1,13 @@
 import { createPopper } from '@popperjs/core'
 import classnames from 'classnames'
-import React, { createRef, useCallback, useState } from 'react'
+import React, { createRef, useCallback, useState, useEffect } from 'react'
 
 export const Dropdown: React.FC<{
   button: React.ReactNode
 }> = ({ button, children }) => {
   const [isOverButton, setIsOverButton] = useState(false)
   const [isOverPopup, setIsOverPopup] = useState(false)
+  const [isLockedOpen, setIsLockedOpen] = useState(false)
 
   const buttonRef = createRef<HTMLDivElement>()
   const popupRef = createRef<HTMLDivElement>()
@@ -30,21 +31,38 @@ export const Dropdown: React.FC<{
     setIsOverPopup(false)
   }, [])
 
+  const handleButtonClick = useCallback(() => {
+    setIsLockedOpen(true)
+  }, [])
+  const handleDocumentClick = useCallback(() => {
+    setIsLockedOpen(false)
+  }, [])
+  useEffect(() => {
+    document.body.addEventListener('click', handleDocumentClick)
+    return () => {
+      document.body.removeEventListener('click', handleDocumentClick)
+    }
+  })
+
+  const isOpen = isOverButton || isOverPopup || isLockedOpen
+
   return (
     <>
       <div
-        className="inline-block"
+        className="inline-block active:opacity-75"
         ref={buttonRef}
         onMouseEnter={handleEnterButton}
         onMouseLeave={handleLeaveButton}
+        onClick={handleButtonClick}
       >
         {button}
       </div>
       <div
         ref={popupRef}
         className={classnames(
-          'bg-white z-30 float-left rounded-sm shadow-lg text-left mt-1 border-gray-200 border select-none',
-          isOverButton || isOverPopup ? 'block' : 'hidden'
+          'bg-white z-30 float-left rounded-sm shadow-lg text-left mt-1',
+          'border-gray-200 border select-none',
+          isOpen ? 'block' : 'hidden'
         )}
         style={{ minWidth: '12rem' }}
         onMouseEnter={handleEnterPopup}
