59cca9b3ee7244bf16cb681e763aee356fb4a4a0
Ian Langworth <ian@langworth.com>
2020-06-17 12:12:34 -0700

    Tooltip style improvements

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -5,6 +5,7 @@ const Tooltip: React.FC<{ tip: string }> = ({ tip, children }) => (
   <TippyTooltip
     title={tip}
     delay={150}
+    duration={0}
     size="small"
     theme="dark"
     className="leading-none"
