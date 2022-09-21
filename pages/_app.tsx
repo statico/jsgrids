e0a72143c5ef7e1688b8d0252c06d67cdad0ac5d
Ian Langworth <ian@langworth.com>
2022-09-20 19:14:12 -0700

    Add CloudFlare analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,17 @@ import { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "756b8ea34ab84ba599f4aca8a95f3137"}'
+      ></script>
+
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   )
 }
 
