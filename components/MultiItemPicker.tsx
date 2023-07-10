f126f1e6e534f7c2aea380a6a13e54e34e9cf9ac
Ian Langworth <ian@langworth.com>
2023-07-10 10:41:48 -0400

    Fix reset button and checkboxes on MultiItemPicker

--- components/MultiItemPicker.tsx
+++ components/MultiItemPicker.tsx
@@ -33,45 +33,47 @@ export const MultiItemPicker = <T extends string>({
   selected,
   options,
   onChange,
-}: MultiItemPickerProps<T>) => (
-  <Menu>
-    <MenuButton as={Button} rightIcon={<GoChevronDown />} fontWeight="normal">
-      {children}
-    </MenuButton>
-    <MenuList p={3}>
-      <Stack>
-        <Text>Choose one or more:</Text>
-        <SimpleGrid columns={2} spacingX={3} spacingY={1}>
-          {options.map(({ key, title, description }) => (
-            <Tooltip key={key} title={description}>
-              <Checkbox
-                checked={selected.has(key)}
-                onChange={(event) => {
-                  const newValue = new Set(selected);
-                  if (event.target.checked) {
-                    newValue.add(key);
-                  } else {
-                    newValue.delete(key);
-                  }
-                  onChange(newValue);
-                }}
-              >
-                {title}
-              </Checkbox>
-            </Tooltip>
-          ))}
-        </SimpleGrid>
-        <Button
-          disabled={selected.size === 0}
-          onClick={() => {
-            onChange(new Set());
-          }}
-        >
-          Reset
-        </Button>
-      </Stack>
-    </MenuList>
-  </Menu>
-);
+}: MultiItemPickerProps<T>) => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<GoChevronDown />} fontWeight="normal">
+        {children}
+      </MenuButton>
+      <MenuList p={3}>
+        <Stack>
+          <Text>Choose one or more:</Text>
+          <SimpleGrid columns={2} spacingX={3} spacingY={1}>
+            {options.map(({ key, title, description }) => (
+              <Tooltip key={key} title={description}>
+                <Checkbox
+                  isChecked={selected.has(key)}
+                  onChange={(event) => {
+                    const newValue = new Set(selected);
+                    if (event.target.checked) {
+                      newValue.add(key);
+                    } else {
+                      newValue.delete(key);
+                    }
+                    onChange(newValue);
+                  }}
+                >
+                  {title}
+                </Checkbox>
+              </Tooltip>
+            ))}
+          </SimpleGrid>
+          <Button
+            disabled={selected.size === 0}
+            onClick={() => {
+              onChange(new Set());
+            }}
+          >
+            Reset
+          </Button>
+        </Stack>
+      </MenuList>
+    </Menu>
+  );
+};
 
 export default MultiItemPicker;
